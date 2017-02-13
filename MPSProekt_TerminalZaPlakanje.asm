3f2dc85a20cadf803200c925fa2cbad0e2798f1a
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2017-02-13 16:05:11 +0100

    First Commit

---
+++ MPSProekt_TerminalZaPlakanje.asm
@@ -0,0 +1,9 @@
+
+_main:
+
+;MPSProekt_TerminalZaPlakanje.c,1 :: 		void main() {
+;MPSProekt_TerminalZaPlakanje.c,2 :: 		char *str = "Hello world";
+;MPSProekt_TerminalZaPlakanje.c,3 :: 		}
+L_end_main:
+	GOTO       $+0
+; end of _main
---
+++ MPSProekt_TerminalZaPlakanje.cp
@@ -0,0 +1,4 @@
+#line 1 "D:/Users/Win 8/Documents/Mikroelektronika/mikroC PRO for PIC/Examples/New folder/MPSProekt_TerminalZaPlakanje.c"
+void main() {
+ char *str = "Hello world";
+}
